8d1f4cd6b417262e5b66517e0444570a955accf9
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2023-07-31 12:42:00 +0300

    KEY

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000;
 
 // Replace with your Supabase credentials
 const supabaseUrl = 'https://qcmamloppghscunvukxc.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjbWFtbG9wcGdoc2N1bnZ1a3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3OTU0OTYsImV4cCI6MjAwNjM3MTQ5Nn0.ijDUlw8KCYvr-C1_qyuizuqeEUaMamAmfw2BQizmrzk'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Define a function to create the table if it doesn't exist
