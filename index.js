0c7ae94473289c80bfb315d929629834d8f90946
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2023-07-31 11:55:32 +0300

    SQLITE3

--- index.js
+++ index.js
@@ -1,117 +1,99 @@
 const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose(); // Import SQLite3 module
 
 const app = express();
 const port = 5000; // You can choose any port you prefer
 
-// MySQL database configuration
-const dbConfig = {
-    user: 'dcutawal', // Replace with your MySQL username (dcutawal@localhost)
-    database: 'dcutawal_enteprenuership', // Replace with your MySQL database name
-    socketPath: '/var/run/mysqld/mysqld.sock', // Replace with the path to your UNIX socket
-  };
-
-// Create a MySQL pool
-const pool = mysql.createPool(dbConfig);
+// SQLite3 database configuration
+const db = new sqlite3.Database('register.db', (err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the SQLite database.');
+  }
+});
 
 // Define a function to create the table if it doesn't exist
-async function createTableIfNotExists() {
-  try {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS registers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(10) NOT NULL,
-        first_name VARCHAR(50) NOT NULL,
-        last_name VARCHAR(50) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        phone_number VARCHAR(20) NOT NULL,
-        micro_church VARCHAR(100),
-        area_of_residence VARCHAR(100),
-        business_interest TEXT
-      )
-    `;
+function createTableIfNotExists() {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS registers (
+      id INTEGER PRIMARY KEY,
+      title TEXT NOT NULL,
+      first_name TEXT NOT NULL,
+      last_name TEXT NOT NULL,
+      email TEXT NOT NULL,
+      phone_number TEXT NOT NULL,
+      micro_church TEXT,
+      area_of_residence TEXT,
+      business_interest TEXT
+    )
+  `;
 
-    pool.query(createTableQuery, (error, results) => {
-      if (error) {
-        console.error('Error creating table:', error);
-      } else {
-        console.log('Table "registers" created (if not existed).');
-      }
-    });
-  } catch (error) {
-    console.error('Error creating table:', error);
-  }
+  db.run(createTableQuery, (err) => {
+    if (err) {
+      console.error('Error creating table:', err.message);
+    } else {
+      console.log('Table "registers" created (if not existed).');
+    }
+  });
 }
 
 // Call the function to create the table at application startup
 createTableIfNotExists();
 
-app.use(cors({
-  origin: '*' // Replace with your frontend's domain or use '*' for any origin
-}));
 app.use(express.json()); // To parse JSON data from requests
 
 // Define a route for handling registration form submission (POST)
-app.post('/register', async (req, res) => {
-  try {
-    const {
-      title,
-      first_name,
-      last_name,
-      email,
-      phone_number,
-      micro_church,
-      area_of_residence,
-      business_interest,
-    } = req.body;
+app.post('/register', (req, res) => {
+  const {
+    title,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    micro_church,
+    area_of_residence,
+    business_interest,
+  } = req.body;
 
-    const query = `
-      INSERT INTO registers (title, first_name, last_name, email, phone_number, micro_church, area_of_residence, business_interest)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+  const insertQuery = `
+    INSERT INTO registers (title, first_name, last_name, email, phone_number, micro_church, area_of_residence, business_interest)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `;
 
-    const values = [
-      title,
-      first_name,
-      last_name,
-      email,
-      phone_number,
-      micro_church,
-      area_of_residence,
-      business_interest,
-    ];
+  const values = [
+    title,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    micro_church,
+    area_of_residence,
+    business_interest,
+  ];
 
-    pool.query(query, values, (error, results) => {
-      if (error) {
-        console.error('Error during registration:', error);
-        res.status(500).json({ error: 'Registration failed.' });
-      } else {
-        res.status(201).json({ message: 'Registration successful!' });
-      }
-    });
-  } catch (error) {
-    console.error('Error during registration:', error);
-    res.status(500).json({ error: 'Registration failed.' });
-  }
+  db.run(insertQuery, values, (err) => {
+    if (err) {
+      console.error('Error during registration:', err.message);
+      res.status(500).json({ error: 'Registration failed.' });
+    } else {
+      res.status(201).json({ message: 'Registration successful!' });
+    }
+  });
 });
 
 // Define a route for fetching registration data (GET)
-app.get('/register', async (req, res) => {
-  try {
-    const query = `SELECT * FROM registers`;
-    pool.query(query, (error, results) => {
-      if (error) {
-        console.error('Error fetching registration data:', error);
-        res.status(500).json({ error: 'Failed to fetch registration data.' });
-      } else {
-        res.status(200).json(results);
-      }
-    });
-  } catch (error) {
-    console.error('Error fetching registration data:', error);
-    res.status(500).json({ error: 'Failed to fetch registration data.' });
-  }
+app.get('/register', (req, res) => {
+  const selectQuery = `SELECT * FROM registers`;
+
+  db.all(selectQuery, (err, rows) => {
+    if (err) {
+      console.error('Error fetching registration data:', err.message);
+      res.status(500).json({ error: 'Failed to fetch registration data.' });
+    } else {
+      res.status(200).json(rows);
+    }
+  });
 });
 
 // Start the server
